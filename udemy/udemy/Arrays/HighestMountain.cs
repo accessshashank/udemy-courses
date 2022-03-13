6a4babc9e79783f32c6d605de21f866db292dc2f
Shashank Shekhar <shekharshashank@outlook.com>
2022-03-13 23:26:29 +0530

    arrays probs

---
+++ udemy/udemy/Arrays/HighestMountain.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy.Arrays
+{
+    class HighestMountain
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {5, 6, 1, 2, 3, 4, 5, 4, 3, 2, 0, 1, 2, 3, -2, 4 };
+
+            Console.WriteLine(Find(arr));
+        }
+
+        private static int Find(int[] arr)
+        {
+            int maxLength = 0;
+            for(int i=0; i<arr.Length;i++)
+            {
+                if(i > 0 && i < arr.Length-1)
+                {
+                    if(arr[i-1] < arr[i] && arr[i] > arr[i+1])
+                    {
+                        // i is the peak
+                        int k = i;
+                        int counter = 1; // because we need to include current element too.
+                        while(k > 0 && arr[k] > arr[k-1])
+                        {
+                            counter++;
+                            k--;
+                        }
+
+                        while(i < arr.Length && arr[i] > arr[i+1])
+                        {
+                            counter++;
+                            i++;
+                        }
+
+                        maxLength = Math.Max(maxLength, counter);
+                    }
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ udemy/udemy/Arrays/LongestBand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy.Arrays
+{
+    class LongestBand
+    {
+        static void Main(string[] args)
+        {
+            // band is subsequence whnich when reordered in such as manner all elements appear consecutive
+            // i.e difference of 1 with neignbouring values
+            int[] arr = new int[] { 1, 9, 3, 0, 18, 5, 2, 4, 10, 7, 12, 6};
+
+            Console.WriteLine(FindLongest(arr));
+        }
+
+        private static int FindLongest(int[] arr)
+        {
+            int maxBand = 0;
+            var dict = new Dictionary<int, int>();
+
+            for(int i=0;i<arr.Length;i++)
+            {
+                dict.Add(arr[i], 0);
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int val = arr[i];
+                if(dict.ContainsKey(val-1))
+                {
+                    continue;
+                }
+                else
+                {
+                    // val can be start of a new band
+                    int counter = 1;
+                    bool found = true;
+                    while(found)
+                    {
+                        val++;
+                        if(dict.ContainsKey(val))
+                        {
+                            counter++;
+                        }
+                        else
+                        {
+                            found = false;
+                        }
+                    }
+
+                    maxBand = Math.Max(maxBand, counter);
+                }
+            }
+            return maxBand;
+        }
+    }
+}
