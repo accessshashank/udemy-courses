1d1c5fc372de33a035fb3271f5d85b229c617d0c
Shashank Shekhar <shekharshashank@outlook.com>
2022-03-24 00:47:14 +0530

    sortsubsequence

---
+++ udemy/udemy/Arrays/SortSubsequence.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy.Arrays
+{
+    class SortSubsequence
+    {
+        static void Main(string[] args)
+        {
+            string input = "abcd";
+            var all = new List<string>();
+            GenerateAndSort("", input, all);
+            Console.WriteLine(string.Join(", ", all));
+        }
+
+        private static void GenerateAndSort(string processed, string unprocessed, List<string> all)
+        {
+            if(string.IsNullOrEmpty(unprocessed))
+            {
+                all.Add(processed);
+                return;
+            }
+
+            char ch = unprocessed[0];
+            GenerateAndSort(processed + ch, unprocessed.Substring(1, unprocessed.Length - 1), all);
+            GenerateAndSort(processed, unprocessed.Substring(1, unprocessed.Length - 1), all);
+            var comparer = new StringComparer();
+            all.Sort(comparer);
+        } 
+    }
+
+    public class StringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x.Length == y.Length)
+            {
+                return x.CompareTo(y);
+            }
+            return x.Length.CompareTo(y.Length);
+        }
+    }
+
+}
