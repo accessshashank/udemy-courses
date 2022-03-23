bebfe1315ee4f1e8996113b56df0a84959643dde
Shashank Shekhar <shekharshashank@outlook.com>
2022-03-24 00:27:16 +0530

    string probs

---
+++ udemy/udemy/Arrays/BusyLife.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy.Arrays
+{
+    class BusyLife
+    {
+        static void Main(string[] args)
+        {
+            var actitities = new List<Tuple<int, int>>();
+            actitities.Add(Tuple.Create(7, 9));
+            actitities.Add(Tuple.Create(0, 10));
+            actitities.Add(Tuple.Create(4, 5));
+            actitities.Add(Tuple.Create(8, 9));
+            actitities.Add(Tuple.Create(4, 10));
+            actitities.Add(Tuple.Create(5, 7));
+
+            Console.WriteLine(MaxActivity(actitities));
+        }
+
+        public class ActivityComparer : IComparer<Tuple<int, int>>
+        {
+            public int Compare(Tuple<int, int> x, Tuple<int, int> y)
+            {
+                return x.Item2.CompareTo(y.Item2);
+            }
+        }
+        private static int MaxActivity(List<Tuple<int, int>> actitities)
+        {
+            actitities.Sort((x, y) => x.Item2.CompareTo(y.Item2));
+            var previousActivity = actitities[0];
+            int counter = 1;
+            for(int i=1; i<actitities.Count;i++)
+            {
+                var currentActivity = actitities[i];
+                if(currentActivity.Item1 >= previousActivity.Item2)
+                {
+                    counter++;
+                    previousActivity = currentActivity;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ udemy/udemy/Arrays/Subsequence.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy.Arrays
+{
+    class Subsequence
+    {
+        static void Main(string[] args)
+        {
+            string original = "codingminutes";
+            string find = "cines";
+            Console.WriteLine(IsSubsequence(original, find));
+            Console.WriteLine(IsSubsequenceInNTime(original, find));
+        }
+
+        private static bool IsSubsequence(string original, string find)
+        {
+            return Subseq(original, 0, find, 0, 0);
+        }
+
+        private static bool Subseq(string original, int i, string find, int j, int count)
+        {
+            if (find.Length == count) return true;
+
+            if (i >= original.Length || j >= find.Length) return false;
+            
+            if(original[i] == find[j])
+            {
+                count++;
+                return Subseq(original, i + 1, find, j + 1, count);
+            }
+
+            return Subseq(original, i + 1, find, j, count) || Subseq(original, i, find, j + 1, count);
+        }
+
+        private static bool IsSubsequenceInNTime(string original, string find)
+        {
+            int i = original.Length - 1;
+            int j = find.Length - 1;
+
+            while(j > -1 && i > -1)
+            {
+                if(original[i] == find[j])
+                {
+                    i--;
+                    j--;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            return j == -1;
+        }
+    }
+}
