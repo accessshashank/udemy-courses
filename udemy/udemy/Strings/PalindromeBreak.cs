ea641d3e962a0a731f8a3f459e44df56581072e7
Shashank Shekhar <shekharshashank@outlook.com>
2022-04-13 00:23:28 +0530

    string probs

---
+++ udemy/udemy/Strings/PalindromeBreak.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy.Strings
+{
+    class PalindromeBreak
+    {
+        static void Main(string[] args)
+        {
+            string ip = "abccba";
+            Console.WriteLine(BreakPalindromeWithLexicographicallySmallest(ip));
+        }
+
+        private static string BreakPalindromeWithLexicographicallySmallest(string ip)
+        {
+            if (ip.Length == 1) return "";
+            int nonA = -1;
+
+            for(int i=0;i<ip.Length;i++)
+            {
+                if(ip[i] != 'a')
+                {
+                    nonA = i;
+                    break;
+                }
+            }
+
+            return ip.Substring(0, nonA) + 'a' + ip.Substring(nonA + 1);
+        }
+    }
+}
