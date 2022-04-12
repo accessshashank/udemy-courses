ceea883e6491840f1f408407405ca2417503b758
Shashank Shekhar <shekharshashank@outlook.com>
2022-04-12 23:54:30 +0530

    string probs

---
+++ udemy/udemy/Strings/RunLengthEncoding.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy.Strings
+{
+    class RunLengthEncoding
+    {
+        static void Main(string[] args)
+        {
+            string ip = "aaaabcccccaaa";
+            Console.WriteLine(Encode(ip));
+        }
+
+        private static string Encode(string ip)
+        {
+
+            string op = string.Empty;
+
+            for(int i=0;i<ip.Length;i++)
+            {
+                int counter = 1;
+                char ch = ip[i];
+                while((i+1 < ip.Length) && ch == ip[i+1])
+                {
+                    counter++;
+                    i = i + 1;
+                }
+                op = op + ch + counter.ToString();
+            }
+
+            if (op.Length <= ip.Length) return op;
+            return ip;
+        }
+    }
+}
